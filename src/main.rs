9b5640bcf1fd1bc131a15d6668468b49e377648d
Quozul <30729291+Quozul@users.noreply.github.com>
2023-04-07 11:55:38 +0200

    Mutex load files

--- src/main.rs
+++ src/main.rs
@@ -5,179 +5,234 @@ use std::fmt::Debug;
 use std::fs;
 use std::fs::File;
 use std::path::PathBuf;
-use std::sync::{Arc, Mutex};
-use std::time::Instant;
+use std::sync::{Arc, Condvar, Mutex};
 use clap::Parser;
-use console::Style;
 use gifski::{Repeat, Settings};
 use gifski::progress::ProgressReporter;
-use indicatif::{HumanDuration, MultiProgress, ProgressBar};
+use indicatif::{MultiProgress, ProgressBar};
 use rayon::prelude::*;
 use crate::gif_progress::{create_style, GifProgress};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-	#[arg(short, long, default_value = "./")]
-	path: String,
-	#[arg(short, long, default_value = "png")]
-	extension: String,
-	#[arg(short, long, default_value = "0.5")]
-	distance: f32,
-	#[arg(short, long, default_value = "90")]
-	quality: u8,
-	#[arg(short, long, default_value = "output.gif")]
-	output: String,
+    #[arg(short, long, default_value = "./")]
+    path: String,
+    #[arg(short, long, default_value = "png")]
+    extension: String,
+    #[arg(short, long, default_value = "0.5")]
+    distance: f32,
+    #[arg(short, long, default_value = "90")]
+    quality: u8,
+    #[arg(short, long, default_value = "output.gif")]
+    output: String,
 }
 
 fn get_image_bytes(path: &PathBuf) -> Vec<u8> {
-	let decoder = png::Decoder::new(File::open(path).unwrap());
-	let mut reader = decoder.read_info().unwrap();
+    let decoder = png::Decoder::new(File::open(path).unwrap());
+    let mut reader = decoder.read_info().unwrap();
 
-	let mut buf = vec![0; reader.output_buffer_size()];
-	reader.next_frame(&mut buf).unwrap();
-	buf
+    let mut buf = vec![0; reader.output_buffer_size()];
+    reader.next_frame(&mut buf).unwrap();
+    buf
 }
 
 // Normalized euclidean distance
 // Thanks ChatGPT
 fn similarity(a: &[u8], b: &[u8]) -> f32 {
-	let mut sum = 0.0;
-	for i in 0..a.len() {
-		let diff = (a[i] as f32 - b[i] as f32) / 255.0;
-		sum += diff * diff;
-	}
-	1.0 - (sum / a.len() as f32).sqrt()
+    let mut sum = 0.0;
+    for i in 0..a.len() {
+        let diff = (a[i] as f32 - b[i] as f32) / 255.0;
+        sum += diff * diff;
+    }
+    1.0 - (sum / a.len() as f32).sqrt()
 }
 
 struct SimilarityScore {
-	start: usize,
-	end: usize,
-	similarity: f32,
-	score: f32,
+    start: usize,
+    end: usize,
+    similarity: f32,
+    score: f32,
+}
+
+struct Data(Vec<u8>);
+
+struct Entry {
+    path: Box<PathBuf>,
+    ready: Mutex<bool>,
+    cvar: Condvar,
+    data: Mutex<Option<Box<Vec<u8>>>>,
+}
+
+impl Entry {
+    fn wait_ready(self: &Entry) -> &Entry {
+        let mut opt = self.ready.lock().unwrap();
+
+        while !*opt {
+            opt = self.cvar.wait(opt).unwrap();
+        }
+
+        self
+    }
+
+    fn get_data(self: &Entry) -> Option<Vec<u8>> {
+        let locked_value = self.data.lock().unwrap();
+
+        match &*locked_value {
+            Some(boxed_vec) => Some((**boxed_vec).clone()),
+            None => None,
+        }
+    }
+}
+
+fn read(element: &Entry) {
+    let mut opt = element.ready.lock().unwrap();
+    while !*opt {
+        opt = element.cvar.wait(opt).unwrap();
+    }
 }
 
 fn main() {
-	let args = Args::parse();
-	let extension = OsStr::new(args.extension.as_str());
+    let args = Args::parse();
+    let extension = OsStr::new(args.extension.as_str());
 
-	let progress_style = create_style();
-	let mut indexing_progress = GifProgress::new(1, String::from("indexing"), 1);
+    let progress_style = create_style();
+    let mut indexing_progress = GifProgress::new(1, String::from("indexing"), 1);
 
-	let mut paths = fs::read_dir(args.path)
-		.unwrap()
-		.filter_map(|entry| {
-			let path = entry.unwrap().path();
-			if path.is_file() && path.extension().unwrap() == extension {
-				Some(path)
-			} else {
-				None
-			}
-		})
-		.collect::<Vec<_>>();
+    let mut paths = fs::read_dir(args.path)
+        .unwrap()
+        .filter_map(|entry| {
+            let path = entry.unwrap().path();
+            if path.is_file() && path.extension().unwrap() == extension {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
 
-	paths.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    paths.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
-	indexing_progress.done(&*format!("indexed {} files", paths.len()));
+    indexing_progress.done(&*format!("indexed {} files", paths.len()));
 
-	let mut loading_files_progress = Arc::new(Mutex::new(GifProgress::new(2, String::from("loading files"), paths.len() as u64)));
+    let loading_files_progress = Arc::new(Mutex::new(GifProgress::new(2, String::from("loading files"), paths.len() as u64)));
 
-	let files = paths.par_iter()
-		.map(|path| {
-			loading_files_progress.lock().unwrap().increase();
-			get_image_bytes(path)
-		})
-		.collect::<Vec<_>>();
+    let files = paths.iter()
+        .map(|path| {
+            Arc::new(Entry {
+                path: Box::new(path.clone()),
+                ready: Mutex::new(false),
+                cvar: Condvar::new(),
+                data: Mutex::new(None),
+            })
+        })
+        .collect::<Vec<_>>();
 
-	loading_files_progress.lock().unwrap().done(&*format!("loaded {} files", paths.len()));
+    let _ = files.par_iter()
+        .for_each(|element| {
+            loading_files_progress.lock().unwrap().increase();
 
-	let length = files.len();
+            let mut opt = element.ready.lock().unwrap();
+            *opt = true;
 
-	let m = MultiProgress::new();
-	let mut diff_pb = Arc::new(Mutex::new(GifProgress::multi(&m, 3, String::from("finding matching frames"), length as u64)));
+            let mut file = element.data.lock().unwrap();
+            *file = Some(Box::new(get_image_bytes(&element.path)));
+            element.cvar.notify_one();
+        });
 
-	// TODO: Compare on GPU
-	let mut matches = files
-		.par_iter()
-		.enumerate()
-		.map(|(i, bytes_a)| {
-			let pb = m.add(ProgressBar::new((length - i) as u64));
-			pb.set_style(progress_style.clone());
+    loading_files_progress.lock().unwrap().done(&*format!("loaded {} files", paths.len()));
 
-			diff_pb.lock().unwrap().increase();
+    let length = paths.len();
 
-			let mut best_difference: f32 = 0.0;
-			let mut best_difference_index: usize = 0;
-			let mut best_score: f32 = 0.0;
+    let m = MultiProgress::new();
+    let diff_pb = Arc::new(Mutex::new(GifProgress::multi(&m, 3, String::from("finding matching frames"), length as u64)));
 
-			for j in (i + 1)..length {
-				pb.set_message(format!("comparing {} and {}", i, j));
-				pb.inc(1);
+    // TODO: Compare on GPU
+    // TODO: Start while loading files
+    let mut matches = files
+        .par_iter()
+        .enumerate()
+        .map(|(i, element)| {
+            let bytes_a = element.wait_ready().get_data().unwrap();
 
-				let bytes_b = &files[j];
-				if bytes_b.len() != bytes_a.len() {
-					continue;
-				}
+            let pb = m.add(ProgressBar::new((length - i) as u64));
+            pb.set_style(progress_style.clone());
 
-				let difference = similarity(&bytes_a, &bytes_b);
+            diff_pb.lock().unwrap().increase();
 
-				// TODO: Update this formula
-				let distance = ((j as f32 - i as f32) / length as f32).sqrt();
-				let score = distance * args.distance + difference * (1.0 - args.distance);
+            let mut best_difference: f32 = 0.0;
+            let mut best_difference_index: usize = 0;
+            let mut best_score: f32 = 0.0;
 
-				if score > best_score {
-					best_difference = difference;
-					best_difference_index = j;
-					best_score = score;
-				}
-			}
+            for j in (i + 1)..length {
+                pb.set_message(format!("comparing {} and {}", i, j));
+                pb.inc(1);
 
-			pb.finish_and_clear();
+                let bytes_b = files[j].wait_ready().get_data().unwrap();
 
-			SimilarityScore {
-				start: i,
-				end: best_difference_index,
-				similarity: best_difference,
-				score: best_score,
-			}
-		})
-		.collect::<Vec<_>>();
+                if bytes_b.len() != bytes_a.len() {
+                    continue;
+                }
 
-	matches.sort_by(|a, b| b.score.partial_cmp(&a.score).unwrap());
+                let difference = similarity(&bytes_a, &bytes_b);
 
-	diff_pb.lock().unwrap().done("calculating differences");
+                // TODO: Update this formula
+                let distance = ((j as f32 - i as f32) / length as f32).sqrt();
+                let score = distance * args.distance + difference * (1.0 - args.distance);
 
-	let best = matches.first().unwrap();
-	let start = best.start;
-	let end = best.end;
-	let fps = 1.0 / 24.0;
-	let frame_count = (end - start) as u64;
+                if score > best_score {
+                    best_difference = difference;
+                    best_difference_index = j;
+                    best_score = score;
+                }
+            }
 
-	let (collector, writer) = gifski::new(Settings {
-		width: None,
-		height: None,
-		quality: args.quality,
-		fast: false,
-		repeat: Repeat::Infinite,
-	}).unwrap();
+            pb.finish_and_clear();
 
-	// TODO: Use multi progress bar
-	let mut collect_files_progress = GifProgress::new(4, String::from("collecting frames"), frame_count);
+            SimilarityScore {
+                start: i,
+                end: best_difference_index,
+                similarity: best_difference,
+                score: best_score,
+            }
+        })
+        .collect::<Vec<_>>();
 
-	let t = std::thread::spawn(move || {
-		for i in start..end {
-			collect_files_progress.increase();
-			let path = &paths[i];
-			collector.add_frame_png_file(i, path.to_path_buf(), fps * i as f64).unwrap();
-		}
+    matches.sort_by(|a, b| b.score.partial_cmp(&a.score).unwrap());
 
-		collect_files_progress.done(&*format!("collected {} frames", frame_count));
-	});
+    diff_pb.lock().unwrap().done("calculating differences");
 
-	let file = File::create(args.output).ok().unwrap();
+    let best = matches.first().unwrap();
+    let start = best.start;
+    let end = best.end;
+    let fps = 1.0 / 24.0;
+    let frame_count = (end - start) as u64;
 
-	let mut reporter = GifProgress::new(5, String::from("making the gif"), frame_count);
-	writer.write(file, &mut reporter).unwrap();
+    let (collector, writer) = gifski::new(Settings {
+        width: None,
+        height: None,
+        quality: args.quality,
+        fast: false,
+        repeat: Repeat::Infinite,
+    }).unwrap();
 
-	t.join().unwrap();
+    let m = MultiProgress::new();
+    let mut collect_files_progress = GifProgress::multi(&m, 4, String::from("collecting frames"), frame_count);
+
+    let t = std::thread::spawn(move || {
+        for i in start..end {
+            collect_files_progress.increase();
+            let path = &paths[i];
+            collector.add_frame_png_file(i, path.to_path_buf(), fps * i as f64).unwrap();
+        }
+
+        collect_files_progress.done(&*format!("collected {} frames", frame_count));
+    });
+
+    let file = File::create(args.output).ok().unwrap();
+
+    let mut reporter = GifProgress::multi(&m, 5, String::from("making the gif"), frame_count);
+    writer.write(file, &mut reporter).unwrap();
+
+    t.join().unwrap();
 }
